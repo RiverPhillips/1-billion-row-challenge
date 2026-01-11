00344e322e1818b1b38365dc711dd30345bd2ca1
River Phillips <riverphillips1@gmail.com>
2026-01-11 09:44:57 +0000

    Revert "perf: attempt to combine delimiter scans in single pass"
    This reverts commit f4a201ff372d2ef844f6aeee835b18406c4270af.

--- main.go
+++ main.go
@@ -177,24 +177,22 @@ func processData(data []byte, start int, endPos int) *hashtable {
 
 	i := start
 	for i < endPos {
-		// Single-pass delimiter scan reduces iterations by ~50%
-		// Find both semicolon and newline in one forward pass
-		semicolonPos := -1
-		lineEnd := i
-		for ; lineEnd < endPos; lineEnd++ {
-			if data[lineEnd] == ';' {
-				semicolonPos = lineEnd
-			} else if data[lineEnd] == '\n' {
-				break
-			}
+		semicolonPos := i
+		for ; semicolonPos < endPos && data[semicolonPos] != ';'; semicolonPos++ {
 		}
-		if semicolonPos == -1 {
+		if semicolonPos == endPos {
 			break
 		}
 
 		hash := hashBytes(data, i, semicolonPos)
 
 		stationKey := data[i:semicolonPos]
+		lineEnd := semicolonPos + 1
+		for ; lineEnd < endPos; lineEnd++ {
+			if data[lineEnd] == '\n' {
+				break
+			}
+		}
 
 		tempStart := semicolonPos + 1
 		tempBytes := data[tempStart:lineEnd]
